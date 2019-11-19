3576cd9d6d76e849813f7f351a00e81e389182f6
akhilmadusudan <akhilmadusudan@gmail.com>
2019-11-19 17:20:43 +0000

    my first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links [Akhil Madusduan & CSP#2]</p>'; ?> 
+    <ul>
+      <li><a href="index.html">index.html</a></li></br>
+     
+      <li><a href="Madusudan_AboutMe.html">AboutMe.html</a></li></br>
+      <li><a href="Madusudan_Projects.html">Projects.html</a></li></br>
+      <li><a href="Madusudan_Resume.html">Resume.html</a></li></br>
+      <li><a href="Madusudan_ContactMe.html">ContactMe.html</a></li></br>
+    <ul>
+  </body>
+</html>
